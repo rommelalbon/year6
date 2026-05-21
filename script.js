6e86743ff84516f678f0e108477586987bd2d44e
RommelAlbon <54616010+rommelalbon@users.noreply.github.com>
2026-05-21 08:24:06 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function checkAnswer(inputId, expected, feedbackId){
- const value=document.getElementById(inputId).value.trim();
- const feedback=document.getElementById(feedbackId);
- feedback.innerHTML=value===expected?'✅ Correct':'❌ Incorrect';
-}
